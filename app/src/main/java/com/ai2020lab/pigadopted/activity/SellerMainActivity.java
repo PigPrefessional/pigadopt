bcdb2e18cbea402f1952b5af42dcf657d406c4d0
justin <zhenghx3@asiainfo.com>
2016-03-08 17:48:13 +0800

    添加卖家主页图片

--- app/src/main/java/com/ai2020lab/pigadopted/activity/SellerMainActivity.java
+++ app/src/main/java/com/ai2020lab/pigadopted/activity/SellerMainActivity.java
@@ -1,16 +1,30 @@
 package com.ai2020lab.pigadopted.activity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
+import com.ai2020lab.aiutils.system.DisplayUtils;
+import com.ai2020lab.pigadopted.R;
 import com.ai2020lab.pigadopted.base.AIBaseActivity;
 
 /**
- * 养猪人主页
+ * 卖家主页
  * Created by Justin on 2016/3/4.
  * Email:502953057@qq.com,zhenghx3@asiainfo.com
  */
 public class SellerMainActivity extends AIBaseActivity {
+	/**
+	 * 日志标题
+	 */
 	private final static String TAG = SellerMainActivity.class.getSimpleName();
+	/**
+	 * 猪圈屋顶布局
+	 */
+	private RelativeLayout hogpenRoofLayout;
+	/**
+	 * 猪圈圈身布局
+	 */
+	private RelativeLayout hogpenBodyLayout;
 
 	/**
 	 * 程序入口
@@ -24,25 +38,16 @@ public class SellerMainActivity extends AIBaseActivity {
 	}
 
 
-	private void init(){
+	private void init() {
 		// 不展示工具栏
 		supportToolbar(false);
+		setContentView(R.layout.activity_seller_main);
+		hogpenRoofLayout = (RelativeLayout) findViewById(R.id.hogpen_roof_layout);
+		hogpenBodyLayout = (RelativeLayout) findViewById(R.id.hogpen_body_layout);
+		RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) hogpenBodyLayout.getLayoutParams();
+		lp.height = (int) (DisplayUtils.getScreenHeight(this) * 0.6);
+		hogpenBodyLayout.setLayoutParams(lp);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
