9e8f96f96022c3a28b86870cbd081a06b9c2fdfa
liliang <liliang9@asiainfo.com>
2016-03-10 11:17:01 +0800

    修改猪详情界面

--- app/src/main/java/com/ai2020lab/pigadopted/fragment/PigDetailFragment.java
+++ app/src/main/java/com/ai2020lab/pigadopted/fragment/PigDetailFragment.java
@@ -1,16 +1,21 @@
 package com.ai2020lab.pigadopted.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.ai2020lab.pigadopted.R;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +26,7 @@ import java.util.List;
 public class PigDetailFragment extends Fragment {
 
     private FrameLayout mPigPartsContainer;
+    private RecyclerView mBuyerRecyclerView;
 
 
     public PigDetailFragment() {
@@ -34,8 +40,10 @@ public class PigDetailFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.fragment_pig_detail, container, false);
 
         mPigPartsContainer = (FrameLayout) rootView.findViewById(R.id.pig_parts_container);
+        mBuyerRecyclerView = (RecyclerView) rootView.findViewById(R.id.buyer_list);
 
         displayPig();
+        loadBuyerList();
 
         return rootView;
     }
@@ -57,6 +65,38 @@ public class PigDetailFragment extends Fragment {
 
         assemblePigParts(partList);
 
+    }
+
+    private void loadBuyerList() {
+        List<Buyer> buyers = new ArrayList<>();
+        Buyer buyer = new Buyer();
+        buyer.portrait = "http://tse4.mm.bing.net/th?id=OIP.Md7bcb36c7db90393682b4bf44487d9f2o0&pid=15.1";
+        buyer.name = "小沈阳";
+        buyers.add(buyer);
+
+        buyer = new Buyer();
+        buyer.portrait = "http://tse4.mm.bing.net/th?id=OIP.Md7bcb36c7db90393682b4bf44487d9f2o0&pid=15.1";
+        buyer.name = "赵四";
+        buyers.add(buyer);
+
+        buyer = new Buyer();
+        buyer.portrait = "http://tse4.mm.bing.net/th?id=OIP.Md7bcb36c7db90393682b4bf44487d9f2o0&pid=15.1";
+        buyer.name = "鸭蛋";
+        buyers.add(buyer);
+
+        buyer = new Buyer();
+        buyer.portrait = "http://tse4.mm.bing.net/th?id=OIP.Md7bcb36c7db90393682b4bf44487d9f2o0&pid=15.1";
+        buyer.name = "张三";
+        buyers.add(buyer);
+
+
+        mBuyerRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mBuyerRecyclerView.setLayoutManager(mLayoutManager);
+        mBuyerRecyclerView.setAdapter(new BuyerAdapter(getContext(), buyers));
     }
 
     private void assemblePigParts(List<PigPart> partList) {
@@ -84,6 +124,62 @@ public class PigDetailFragment extends Fragment {
     private class PigPart {
         String name;
         String partId;
+    }
+
+    private class Buyer {
+        String name;
+        String portrait;
+    }
+
+    private class BuyerAdapter extends
+            RecyclerView.Adapter<BuyerAdapter.ViewHolder> {
+
+        private LayoutInflater mInflater;
+        private List<Buyer> mBuyers;
+
+        public BuyerAdapter(Context context, List<Buyer> buyers) {
+            mInflater = LayoutInflater.from(context);
+            mBuyers = buyers;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            public ViewHolder(View view) {
+                super(view);
+            }
+
+            ImageView mPortrait;
+            TextView mName;
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBuyers.size();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = mInflater.inflate(R.layout.item_buyer,
+                    viewGroup, false);
+            ViewHolder viewHolder = new ViewHolder(view);
+
+            viewHolder.mPortrait = (ImageView) view
+                    .findViewById(R.id.buyer_portrait);
+            viewHolder.mName = (TextView) view.findViewById(R.id.buyer_name);
+
+            return viewHolder;
+        }
+
+
+        @Override
+        public void onBindViewHolder(final ViewHolder viewHolder, final int index) {
+            final ImageLoader imageLoader = ImageLoader.getInstance();
+
+            final Buyer buyer = mBuyers.get(index);
+
+            viewHolder.mName.setText(buyer.name);
+            imageLoader.displayImage(buyer.portrait, viewHolder.mPortrait);
+        }
+
     }
 
 }
