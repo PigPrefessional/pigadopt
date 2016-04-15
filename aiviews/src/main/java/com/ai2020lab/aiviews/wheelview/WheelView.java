cd424a61979a97ef7f08a70c452c2bf7f846c763
justin <zhenghx3@asiainfo.com>
2016-04-15 16:49:05 +0800

    修改添加猪和添加猪圈界面

--- aiviews/src/main/java/com/ai2020lab/aiviews/wheelview/WheelView.java
+++ aiviews/src/main/java/com/ai2020lab/aiviews/wheelview/WheelView.java
@@ -67,7 +67,7 @@ public class WheelView extends View {
 	int dividerColor;
 
 	// 条目间距倍数
-	static final float lineSpacingMultiplier = 1.0F;
+	static final float lineSpacingMultiplier = 1.1F;
 	boolean isLoop;
 
 	// 第一条线Y坐标值
